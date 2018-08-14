d3999a8475cfc80417a4208188748643bdf7e523
Jianhui Zhao <jianhuizhao329@gmail.com>
2018-08-15 00:40:08 +0800

    icons: Only use woff2 from material-design-icons
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- vuci-ui-base/src/src/main.js
+++ vuci-ui-base/src/src/main.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
-import 'material-design-icons/iconfont/material-icons.css'
+import './material-design-icons/material-icons.css'
 
 import './plugins/vuetify'
 import VueFilter from './plugins/vue-filter.js'
---
+++ vuci-ui-base/src/src/material-design-icons/README.md
@@ -0,0 +1,3 @@
+Import from http://google.github.io/material-design-icons/ v3.0.1
+
+Only keep the woff2.
