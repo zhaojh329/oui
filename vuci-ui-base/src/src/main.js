60d8a4682662154c9f3be26b517778e195c3bd72
Jianhui Zhao <jianhuizhao329@gmail.com>
2019-05-17 17:39:38 +0800

    Use vue-axios
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- vuci-ui-base/src/src/main.js
+++ vuci-ui-base/src/src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
-import './plugins/axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './plugins/iview.js'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
