17f972f9ac96efbc990b89aa04894765c47fc5fa
Jianhui Zhao <jianhuizhao329@gmail.com>
2019-05-22 19:48:48 +0800

    eslint: configure eslintrc
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- vwrt-ui-core/src/.eslintrc.js
+++ vwrt-ui-core/src/.eslintrc.js
@@ -9,7 +9,24 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'linebreak-style': ['error', 'unix'],
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'brace-style': 'error',
+    'comma-dangle': 'error',
+    'comma-spacing': 'error',
+    'keyword-spacing': 'error',
+    'no-trailing-spaces': 'error',
+    'no-unneeded-ternary': 'error',
+    'space-before-function-paren': ['error', 'never'],
+    'space-infix-ops': ['error', {'int32Hint': false}],
+    'arrow-spacing': 'error',
+    'no-var': 'error',
+    'no-duplicate-imports': 'error',
+    'space-before-blocks': 'error',
+    'space-in-parens': ["error", 'never'],
+    'no-multi-spaces': 'error'
   },
   parserOptions: {
     parser: 'babel-eslint'
