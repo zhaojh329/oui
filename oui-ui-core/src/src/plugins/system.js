742bc05ed761df3fe66e7bc54eeb3de3e953dbf2
Jianhui Zhao <jianhuizhao329@gmail.com>
2019-05-26 22:08:22 +0800

    plugin:system: new function for init
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- oui-ui-core/src/src/plugins/system.js
+++ oui-ui-core/src/src/plugins/system.js
@@ -40,6 +40,52 @@ system.getInfo = function() {
   });
 }
 
+system.initList = function() {
+  return new Promise(resolve => {
+    ubus.call('oui.system', 'init_list').then(r => {
+      resolve(r.initscripts);
+    });
+  });
+}
+
+system.initEnabled = function(name) {
+  return new Promise(resolve => {
+    this.initList().then(list => {
+      for (let i = 0; i < list.length; i++) {
+        if (list[i].name === name) {
+          resolve(list[i].enabled);
+          return;
+        }
+      }
+      resolve(false);
+    });
+  });
+}
+
+system.initRun = function(name, action) {
+  return ubus.call('oui.system', 'init_action', {name, action});
+}
+
+system.initStart = function(name) {
+  return this.initRun(name, 'start');
+}
+
+system.initStop = function(name) {
+  return this.initRun(name, 'stop');
+}
+
+system.initRestart = function(name) {
+  return this.initRun(name, 'restart');
+}
+
+system.initEnable = function(name) {
+  return this.initRun(name, 'enable');
+}
+
+system.initDisable = function(name) {
+  return this.initRun(name, 'disable');
+}
+
 export default {
   install(Vue) {
     Vue.prototype.$system = system;
