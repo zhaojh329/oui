0f6e44068437170a6fb3a0333df8e9ee41af6430
Jianhui Zhao <jianhuizhao329@gmail.com>
2019-05-28 12:59:00 +0800

    UciMap: Drop save button
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- oui-ui-core/src/src/components/UciMap.vue
+++ oui-ui-core/src/src/components/UciMap.vue
@@ -3,7 +3,6 @@
     <slot></slot>
     <div style="position:absolute; right: 100px">
       <Button type="primary" style="margin-right: 10px" @click="apply">Save &  Apply</Button>
-      <Button type="info" style="margin-right: 10px" @click="save">Save</Button>
       <Button type="warning" @click="reset">Reset</Button>
     </div>
   </div>
@@ -20,19 +19,13 @@ export default {
       this.$uci.save().then(() => {
         this.$uci.apply().then(() => {
           this.load();
-          this.$Message.success('Apply successfully');
+          this.$Message.success('Save & Apply successfully');
         }).catch(e => {
           if (e.code === 5)
             this.$Message.warning('There are no changes to apply');
           else
             throw e;
         });
-      });
-    },
-    save() {
-      this.$uci.save().then(() => {
-        this.load();
-        this.$Message.success('Save successfully');
       });
     },
     reset() {
@@ -49,3 +42,4 @@ export default {
   }
 }
 </script>
+
