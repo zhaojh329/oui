c10c538d089b4a042f3e6e3453ae326cd3db4feb
Jianhui Zhao <jianhuizhao329@gmail.com>
2019-06-08 21:35:56 +0800

    Optimize code
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- oui-ui-core/src/src/components/uci-form/src/uci-form-item.vue
+++ oui-ui-core/src/src/components/uci-form/src/uci-form-item.vue
@@ -1,12 +1,12 @@
 <template>
   <el-form-item :label="option.label" :prop="prop" v-if="option.show">
-    <el-input v-if="option.type === 'input'" v-model="form[prop]" :placeholder="option.placeholder"></el-input>
-    <el-input v-else-if="option.type === 'dummy'" :value="form[prop]" readonly></el-input>
-    <el-switch v-else-if="option.type === 'switch'" v-model="form[prop]" :active-value="option.activeValue" :inactive-value="option.inactiveValue"></el-switch>
-    <el-select v-else-if="option.type === 'list'" v-model="form[prop]" :clearable="!option.required">
+    <el-input v-if="type === 'input'" v-model="form[prop]" :placeholder="option.placeholder"></el-input>
+    <el-input v-else-if="type === 'dummy'" :value="form[prop]" readonly></el-input>
+    <el-switch v-else-if="type === 'switch'" v-model="form[prop]" :active-value="option.activeValue" :inactive-value="option.inactiveValue"></el-switch>
+    <el-select v-else-if="type === 'list'" v-model="form[prop]" :clearable="!option.required">
     <el-option v-for="oo in option.transformedOptions" :key="oo[0]" :label="oo[1] || oo[0]" :value="oo[0]"></el-option>
     </el-select>
-    <uci-dlist v-else-if="option.type === 'dlist'" v-model="form[prop]"></uci-dlist>
+    <uci-dlist v-else-if="type === 'dlist'" v-model="form[prop]"></uci-dlist>
   </el-form-item>
 </template>
 
@@ -16,12 +16,20 @@ import UciDlist from './uci-dlist'
 export default {
   name: 'UciFormItem',
   props: {
+    sid: String,
     option: Object,
-    prop: String,
     form: Object
   },
   components: {
     UciDlist
+  },
+  computed: {
+    prop() {
+      return this.option.prop(this.sid);
+    },
+    type() {
+      return this.option.type;
+    }
   }
 }
 </script>
