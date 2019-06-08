bf71d574a4adbdfdadb0039409feb9e66c016df5
Jianhui Zhao <jianhuizhao329@gmail.com>
2019-06-08 22:42:03 +0800

    network.js: support getDevices
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- oui-ui-core/src/src/plugins/network.js
+++ oui-ui-core/src/src/plugins/network.js
@@ -2,12 +2,27 @@
 
 import {ubus} from './ubus'
 
-const network = {}
+const network = {
+  interfaces: [],
+  devices: []
+}
 
 network.load = function() {
+  const promises = [];
+
+  promises.push(ubus.call('network.device', 'status'));
+  promises.push(ubus.call('network.interface', 'dump'));
+
   return new Promise(resolve => {
-    ubus.call('network.interface', 'dump').then(r => {
-      this.interfaces = r.interface;
+    Promise.all(promises).then(r => {
+      const devices = r[0];
+      this.devices = [];
+      Object.keys(devices).forEach(name => {
+        this.devices.push({name: name, ...devices[name]});
+      });
+
+      this.interfaces = r[1].interface;
+
       resolve();
     });
   });
@@ -17,7 +32,7 @@ network.getInterface = function(name) {
   const interfaces = this.interfaces;
 
   for (let i = 0; i < interfaces.length; i++) {
-    if (interfaces[i].name === name)
+    if (interfaces[i].interface === name)
       return interfaces[i];
   }
 
@@ -26,6 +41,21 @@ network.getInterface = function(name) {
 
 network.getInterfaces = function() {
   return this.interfaces;
+}
+
+network.getDevice = function(name) {
+  const devices = this.devices;
+
+  for (let i = 0; i < devices.length; i++) {
+    if (devices[i].name === name)
+      return devices[i];
+  }
+
+  return null;
+}
+
+network.getDevices = function() {
+  return this.devices;
 }
 
 export default {
