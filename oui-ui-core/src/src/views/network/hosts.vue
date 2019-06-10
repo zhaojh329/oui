de1d035ff10bd09101a29a0f9f304fb16f4ff74b
Jianhui Zhao <jianhuizhao329@gmail.com>
2019-06-10 16:45:08 +0800

    hosts.vue: new page
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

---
+++ oui-ui-core/src/src/views/network/hosts.vue
@@ -0,0 +1,18 @@
+<template>
+  <uci-form config="dhcp" @apply="onApply">
+    <uci-section title="Hostnames" type="domain" addable>
+      <uci-option type="input" label="Hostname" name="name" required></uci-option>
+      <uci-option type="input" label="IP address" name="ip" required></uci-option>
+    </uci-section>
+  </uci-form>
+</template>
+
+<script>
+export default {
+  methods: {
+    onApply() {
+      this.$system.initRestart('dnsmasq');
+    }
+  }
+}
+</script>
