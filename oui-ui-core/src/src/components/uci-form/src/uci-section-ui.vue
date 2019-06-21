f77b86bfd6350445a6b0747b4fdd8beaee157c99
Jianhui Zhao <jianhuizhao329@gmail.com>
2019-06-22 01:20:50 +0800

    Fix css
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- oui-ui-core/src/src/components/uci-form/src/uci-section-ui.vue
+++ oui-ui-core/src/src/components/uci-form/src/uci-section-ui.vue
@@ -1,17 +1,21 @@
 <template>
   <div>
-    <uci-section-del :sestion="sestion" :sid="sid"></uci-section-del>
-    <el-tabs v-if="tabs.length > 0" :value="tabs[0].name">
-      <template v-for="tab in tabs">
-        <el-tab-pane v-if="tab.options.length > 0" :key="tab.name" :name="tab.name">
-          <span slot="label">
-            {{ tab.title }}<el-badge v-if="tab.getErrorNum(sid) > 0" :value="tab.getErrorNum(sid)"></el-badge>
-          </span>
-          <uci-form-item v-for="o in tab.options" :key="o.name" :option="o" :sid="sid"></uci-form-item>
-        </el-tab-pane>
-      </template>
-    </el-tabs>
-    <uci-form-item v-for="o in noTabOptions" :key="o.name" :option="o" :sid="sid"></uci-form-item>
+    <div class="oui-uci-section-ui">
+      <div style="width: 100%">
+        <el-tabs v-if="tabs.length > 0" :value="tabs[0].name">
+          <template v-for="tab in tabs">
+            <el-tab-pane v-if="tab.options.length > 0" :key="tab.name" :name="tab.name">
+              <span slot="label">
+                {{ tab.title }}<el-badge v-if="tab.getErrorNum(sid) > 0" :value="tab.getErrorNum(sid)"></el-badge>
+              </span>
+              <uci-form-item v-for="o in tab.options" :key="o.name" :option="o" :sid="sid"></uci-form-item>
+            </el-tab-pane>
+          </template>
+        </el-tabs>
+        <uci-form-item v-for="o in noTabOptions" :key="o.name" :option="o" :sid="sid"></uci-form-item>
+      </div>
+      <uci-section-del :sestion="sestion" :sid="sid"></uci-section-del>
+    </div>
     <el-divider v-if="divider"></el-divider>
   </div>
 </template>
@@ -42,3 +46,12 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.oui-uci-section-ui {
+  display: flex;
+  .el-button {
+    height: 28px;
+  }
+}
+</style>
