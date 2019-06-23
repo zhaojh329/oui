fd5bb1b659666f122ddbb62c3912834e73156cbe
Jianhui Zhao <jianhuizhao329@gmail.com>
2019-06-23 12:25:12 +0800

    uci-form: Support tabbed section
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- oui-ui-core/src/src/components/uci-form/src/uci-form.vue
+++ oui-ui-core/src/src/components/uci-form/src/uci-form.vue
@@ -2,7 +2,44 @@
   <div class="uci-form">
     <div class="hidden-Section"><slot></slot></div>
     <el-form class="form" label-width="280px" ref="form" :model="form" :rules="rules" @validate="onValidate" :disabled="readonly">
-      <template v-for="(s, i) in sections">
+      <el-tabs v-if="tabbed" value="0">
+        <el-tab-pane v-for="(s, i) in sections" :key="i" :name="i + ''" :label="s.title">
+          <el-table v-if="s.table" :data="s.sids">
+            <el-table-column v-for="o in s.arrayedOptions" :key="o.name" :label="o.label" :width="o.width">
+              <template slot="header" v-if="o.header">
+                <span v-html="o.header"></span>
+              </template>
+              <template v-slot="{ row }">
+                <uci-form-item :option="o" :sid="row" :form="form" table></uci-form-item>
+              </template>
+            </el-table-column>
+            <el-table-column v-if="s.addable && s.type && !s.name" :width="s.tableActionWidth">
+              <template v-slot="{ row }">
+                <uci-section-table-action-ui style="margin-bottom: 22px" :section="s" :sid="row"></uci-section-table-action-ui>
+              </template>
+            </el-table-column>
+          </el-table>
+          <el-collapse v-else-if="s.collapse" v-model="activeCollapseItem" accordion>
+            <el-collapse-item v-for="sid in s.sids" :key="sid" :title="sid" :name="sid">
+              <template slot="title">
+                <span v-if="activeCollapseItem === sid"></span>
+                <span v-else>
+                  <el-badge :key="'badge' + i" v-if="s.getErrorNum(sid) > 0" :value="s.getErrorNum(sid)"></el-badge>
+                  <template v-for="(t, i) in s.teasersValue(sid)">
+                    {{ t[0] }}: <strong :key="i">{{ t[1] }}</strong>{{ t[2] ? '' : ' | ' }}
+                  </template>
+                </span>
+              </template>
+              <uci-section-ui :sestion="s" :sid="sid"></uci-section-ui>
+            </el-collapse-item>
+          </el-collapse>
+          <div v-else v-for="(sid, i) in s.sids" :key="sid">
+            <uci-section-ui :sestion="s" :sid="sid" :divider="i < s.sids.length - 1"></uci-section-ui>
+          </div>
+          <uci-section-add :sestion="s"></uci-section-add>
+        </el-tab-pane>
+      </el-tabs>
+      <template v-else v-for="(s, i) in sections">
         <el-card :key="i" :header="s.title">
           <el-table v-if="s.table" :data="s.sids">
             <el-table-column v-for="o in s.arrayedOptions" :key="o.name" :label="o.label" :width="o.width">
@@ -64,7 +101,8 @@ export default {
     config: {
       type: String,
       required: true
-    }
+    },
+    tabbed: Boolean
   },
   components: {
     UciFormItem,
