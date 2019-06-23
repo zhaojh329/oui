403faa1c32bb0fde1588844534dd67f936e7fa32
Jianhui Zhao <jianhuizhao329@gmail.com>
2019-06-23 23:05:56 +0800

    firewall: Add rule config (ugly)
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- oui-ui-core/src/src/views/network/firewall.vue
+++ oui-ui-core/src/src/views/network/firewall.vue
@@ -38,6 +38,20 @@
           </el-button-group>
         </template>
       </uci-section>
+      <uci-section type="rule" :title="$t('Traffic Rules')" table addable table-action-width="140" :add="addRule">
+        <uci-option-input :label="$t('Name')" name="name"></uci-option-input>
+        <uci-option-list :label="$t('Protocol')" name="proto" :options="protos" initial="tcp udp" allow-create></uci-option-list>
+        <uci-option-list :label="$t('Source zone')" name="src" :options="zones"></uci-option-list>
+        <uci-option-list :label="$t('Internal zone')" name="dest" :options="zones"></uci-option-list>
+        <uci-option-input :label="$t('Destination port')" name="dest_port" :placeholder="$t('any')" rules="port" depend="proto == 'tcp' || proto == 'udp' || proto == 'tcp udp'"></uci-option-input>
+        <uci-option-list :label="$t('Action')" name="target" :options="actions" initial="DROP" required></uci-option-list>
+        <template v-slot:action="{s, sid}">
+          <el-button-group>
+            <el-button type="primary" size="mini" @click="editRule(sid)">{{ $t('Edit') }}</el-button>
+            <el-button type="danger" size="mini" @click="s.del(sid)">{{ $t('Delete') }}</el-button>
+          </el-button-group>
+        </template>
+      </uci-section>
     </uci-form>
     <el-dialog :title="dialogZoneTitle" :visible.sync="dialogZoneVisible" custom-class="firewall-edit-dialog">
       <uci-form config="firewall" v-if="dialogZoneVisible" @apply="onApply">
@@ -83,6 +97,25 @@
         </uci-section>
       </uci-form>
     </el-dialog>
+    <el-dialog :title="dialogRuleTitle" :visible.sync="dialogRuleVisible" custom-class="firewall-edit-dialog">
+      <uci-form config="firewall" v-if="dialogRuleVisible" @apply="onApply">
+        <uci-section :name="editorRuleSid">
+          <uci-option-input :label="$t('Name')" name="name"></uci-option-input>
+          <uci-option-list :label="$t('Protocol')" name="proto" :options="protos" initial="tcp udp" allow-create></uci-option-list>
+          <uci-option-list :label="$t('Source zone')" name="src" :options="zones"></uci-option-list>
+          <uci-option-list :label="$t('Internal zone')" name="dest" :options="zones"></uci-option-list>
+          <uci-option-input :label="$t('Destination port')" name="dest_port" :placeholder="$t('any')" rules="port" depend="proto == 'tcp' || proto == 'udp' || proto == 'tcp udp'"></uci-option-input>
+          <uci-option-list :label="$t('Week Days')" name="weekdays" :options="weekdays" multiple></uci-option-list>
+          <uci-option-list :label="$t('Month Days')" name="monthdays" :options="monthdays" multiple></uci-option-list>
+          <uci-option-input :label="$t('Start Time (hh:mm:ss)')" name="start_time"></uci-option-input>
+          <uci-option-input :label="$t('Stop Time (hh:mm:ss)')" name="stop_time"></uci-option-input>
+          <uci-option-input :label="$t('Start Date (yyyy-mm-dd)')" name="start_date"></uci-option-input>
+          <uci-option-input :label="$t('Stop Date (yyyy-mm-dd)')" name="stop_date"></uci-option-input>
+          <uci-option-switch :label="$t('Time in UTC')" name="utc_time"></uci-option-switch>
+          <uci-option-list :label="$t('Action')" name="target" :options="actions" initial="DROP" required></uci-option-list>
+        </uci-section>
+      </uci-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -112,7 +145,25 @@ export default {
       ],
       arps: [],
       editorDnatSid: '',
-      dialogDnatVisible: false
+      dialogDnatVisible: false,
+      editorRuleSid: '',
+      dialogRuleVisible: false,
+      actions: [
+        ['DROP', this.$t('drop')],
+        ['ACCEPT', this.$t('accept')],
+        ['REJECT', this.$t('reject')],
+        ['NOTRACK', this.$t('don\'t track')]
+      ],
+      weekdays: [
+        ['Sun', this.$t('Sunday')],
+        ['Mon', this.$t('Monday')],
+        ['Tue', this.$t('Tuesday')],
+        ['Wed', this.$t('Wednesday')],
+        ['Thu', this.$t('Thursday')],
+        ['Fri', this.$t('Friday')],
+        ['Sat', this.$t('Saturday')]
+      ],
+      monthdays: []
     }
   },
   computed: {
@@ -126,6 +177,12 @@ export default {
         return '';
       const name = this.$uci.get('firewall', this.editorDnatSid, 'name') || '';
       return `${this.$t('Port Forwards')} "${name}"`
+    },
+    dialogRuleTitle() {
+      if (!this.editorRuleSid)
+        return '';
+      const name = this.$uci.get('firewall', this.editorRuleSid, 'name') || '';
+      return `${this.$t('Traffic Rules')} "${name}"`
     }
   },
   methods: {
@@ -168,6 +225,10 @@ export default {
     editDnat(sid) {
       this.editorDnatSid = sid;
       this.dialogDnatVisible = true;
+    },
+    editRule(sid) {
+      this.editorRuleSid = sid;
+      this.dialogRuleVisible = true;
     },
     onApply() {
       this.$refs['form'].reset();
@@ -247,6 +308,25 @@ export default {
         });
       });
     },
+    addRule() {
+      const loading = this.$getLoading(this.$t('Loading...'));
+
+      const sid = this.$uci.add('firewall', 'rule');
+
+      this.$uci.set('firewall', sid, 'target', 'ACCEPT');
+      this.$uci.set('firewall', sid, 'src', 'wan');
+      this.$uci.set('firewall', sid, 'proto', 'tcp udp');
+
+      this.$uci.save().then(() => {
+        this.$uci.apply().then(() => {
+          this.$uci.load('firewall').then(() => {
+            this.$refs['form'].reset();
+            this.load();
+            loading.close();
+          });
+        });
+      });
+    },
     getARPTable() {
       return this.$ubus.call('oui.network', 'arp_table');
     },
@@ -272,6 +352,9 @@ export default {
         this.arps.push([arp.ipaddr, `${arp.ipaddr} (${arp.macaddr})`]);
       });
     });
+
+    for (let i = 1; i <= 31; i++)
+      this.monthdays.push(i);
   }
 }
 </script>
