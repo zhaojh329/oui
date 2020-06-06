c94dd1515028e658df6b639779793e09a91f962d
sbilly <superli_1980@hotmail.com>
2020-06-06 17:41:43 +0800

    Update vue.config.js
    用当前时间作为标签，区分每次打包输出的文件，从而防止新打包的文件被客户端缓存

--- oui-ui-core/src/vue.config.js
+++ oui-ui-core/src/vue.config.js
@@ -1,3 +1,5 @@
+const Timestamp = new Date().getTime(); // 用当前时间作为标签，区分每次打包输出的文件，从而防止新打包的文件被客户端缓存
+
 module.exports = {
   indexPath: 'oui.html',
   productionSourceMap: false,
@@ -23,6 +25,12 @@ module.exports = {
       fallbackLocale: 'en',
       localeDir: 'locales',
       enableInSFC: false
+    }
+  },
+  configureWebpack: {
+    output: { // 输出重构打包编译后的文件名，增加 Timestamp，从而防止新打包的文件被客户端缓存
+      filename: `js/[name].[chunkhash:8].${Timestamp}.js`,
+      chunkFilename: `js/[name].[chunkhash:8].${Timestamp}.js`
     }
   }
 }
