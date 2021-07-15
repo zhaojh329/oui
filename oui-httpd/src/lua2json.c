f4c3d6daf83732a64e74908b75548583ff8b4f4b
Jianhui Zhao <zhaojh329@gmail.com>
2021-07-15 16:45:24 +0800

    oui-httpd: lua: fix lua to json
    encode_empty_table_as_array have no effect for nested table
    
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- oui-httpd/src/lua2json.c
+++ oui-httpd/src/lua2json.c
@@ -55,9 +55,9 @@ out:
     return max;
 }
 
-static void encode(lua_State *L, json_t *ret);
+static void encode(lua_State *L, json_t *ret, bool encode_empty_table_as_array);
 
-static json_t *append_data(lua_State *L)
+static json_t *append_data(lua_State *L, bool encode_empty_table_as_array)
 {
     lua_Number nd, ni;
     int array_size;
@@ -80,19 +80,19 @@ static json_t *append_data(lua_State *L)
     case LUA_TTABLE:
         array_size = lua_test_array(L);
 
-        if (array_size >= 0) {
+        if (array_size > 0 || (array_size == 0 && encode_empty_table_as_array)) {
             json_t *a = json_array();
             int i;
 
             for (i = 1; i <= array_size; i++) {
                 lua_rawgeti(L, -1, i);
-                json_array_append_new(a, append_data(L));
+                json_array_append_new(a, append_data(L, encode_empty_table_as_array));
                 lua_pop(L, 1);
             }
             return a;
         } else {
             json_t *o = json_object();
-            encode(L, o);
+            encode(L, o, encode_empty_table_as_array);
             return o;
         }
     default:
@@ -101,7 +101,7 @@ static json_t *append_data(lua_State *L)
     }
 }
 
-static void encode(lua_State *L, json_t *ret)
+static void encode(lua_State *L, json_t *ret, bool encode_empty_table_as_array)
 {
     lua_pushnil(L);
 
@@ -122,7 +122,7 @@ static void encode(lua_State *L, json_t *ret)
             return;
         }
 
-        json_object_set_new(ret, key, append_data(L));
+        json_object_set_new(ret, key, append_data(L, encode_empty_table_as_array));
 
         lua_pop(L, 1);
     }
@@ -142,12 +142,12 @@ json_t *lua_to_json(lua_State *L, int i, bool encode_empty_table_as_array)
 
             for (i = 1; i <= array_size; i++) {
                 lua_rawgeti(L, -1, i);
-                json_array_append_new(ret, append_data(L));
+                json_array_append_new(ret, append_data(L, encode_empty_table_as_array));
                 lua_pop(L, 1);
             }
         } else {
             ret = json_object();
-            encode(L, ret);
+            encode(L, ret, encode_empty_table_as_array);
         }
     } else {
         ret = json_object();
@@ -184,7 +184,6 @@ static void decode_append(lua_State *L, json_t *val)
         break;
     case JSON_ARRAY:
         lua_newtable(L);
-
         json_array_foreach(val, i, aval) {
             decode_append(L, aval);
             lua_rawseti(L, -2, i + 1);
