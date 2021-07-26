50843c13dca31376a0a58f691d6eab1f734aa93c
Jianhui Zhao <zhaojh329@gmail.com>
2021-07-26 03:10:53 +0000

    oui-httpd: lua: improve utils.readfile
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- oui-httpd/src/lua/utils.lua
+++ oui-httpd/src/lua/utils.lua
@@ -6,12 +6,31 @@ setmetatable(M, {
     __index = core
 })
 
-M.readfile = function(name, n)
+-- The available formats are:
+-- "n": reads a numeral and returns it as a float or an integer, following the lexical conventions of Lua.
+-- "a": reads the whole file. This is the default format.
+-- "l": reads the next line skipping the end of line.
+-- "L": reads the next line keeping the end-of-line character (if present).
+-- number: reads a string with up to this number of bytes. If number is zero, it reads nothing and returns an empty string.
+-- Return nil if the file open failed
+M.readfile = function(name, format)
     local f = io.open(name, "r")
     if not f then return nil end
-    local data = f:read(n or "*a")
+
+    -- Compatible with the version below 5.3
+    if type(format) == "string" and format:sub(1, 1) ~= "*" then format = "*" .. format end
+
+    local data
+
+    if format == "*L" and tonumber(_VERSION:match("%d.%d")) < 5.2 then
+        data = f:read("*l")
+        if data then data = data .. "\n" end
+    else
+        data = f:read(format or "*a")
+    end
+
     f:close()
-    return data
+    return data or ""
 end
 
 M.writefile = function (name, data, append)
