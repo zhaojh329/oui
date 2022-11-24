b11ef31f23bc8ddb6e401339761c486e6fa8c197
Jianhui Zhao <zhaojh329@gmail.com>
2022-11-25 00:01:28 +0800

    check node version
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- node.mk
+++ node.mk
@@ -1,5 +1,30 @@
+CL_RED=$(shell tput setaf 1)
+CL_CYN=$(shell tput setaf 6)
+CL_RST=$(shell tput sgr0)
+
+define ColorInfo
+  $(info $(CL_CYN)$1$(CL_RST))
+endef
+
+define ColorError
+  $(error $(CL_RED)$1$(CL_RST))
+endef
+
 ifneq ($(CONFIG_OUI_USE_HOST_NODE),)
-NPM := PATH=/usr/local/bin:/usr/bin npm
+OUI_NODE_PATH := PATH=/usr/local/bin:/usr/bin
+NODE := $(OUI_NODE_PATH) node
+NPM := $(OUI_NODE_PATH) npm
+$(eval $(call ColorInfo,Using Node.js from Host))
 else
+NODE := node
 NPM := npm
+$(eval $(call ColorInfo,Using Node.js from OpenWrt))
+endif
+
+NODE_VER := $(shell $(NODE) -v | sed 's/v//' | awk -F. '{print $$1"."$$2}')
+
+$(eval $(call ColorInfo,Node.js version: $(NODE_VER)))
+
+ifneq ($(shell echo ${NODE_VER} | awk '{if ($$1 >= 14.18) { print "y" }}'),y)
+$(eval $(call ColorError,Node.js 14.18+ is required))
 endif
