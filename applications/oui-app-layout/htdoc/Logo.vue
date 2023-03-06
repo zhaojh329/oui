29011d3adbfeb3330af9885519fd0c7f2eecb66c
Chao Liu <liuchao126mail@126.com>
2023-03-06 13:35:56 +0800

    oui-app-layout: make sider responsive
    Signed-off-by: Chao Liu <liuchao126mail@126.com>

---
+++ applications/oui-app-layout/htdoc/Logo.vue
@@ -0,0 +1,52 @@
+<template>
+  <router-link to="/" style="text-decoration: none;" :class="{ dark: $oui.state.theme === 'dark' }">
+    <div class="logo">
+      <img :class="{'mr-2': !collapsed}"/>
+      <h2 v-show="!collapsed" class="title">{{ $oui.state.hostname }}</h2>
+    </div>
+  </router-link>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Index',
+  props: {
+    collapsed: {
+      type: Boolean
+    }
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.logo {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 64px;
+  line-height: 64px;
+  overflow: hidden;
+  white-space: nowrap;
+}
+.logo img {
+  width: auto;
+  height: 32px;
+  content: url(./openwrt-logo-black.svg);
+}
+.dark .logo img {
+  width: auto;
+  height: 32px;
+  content: url(./openwrt-logo-white.svg);
+}
+.logo .title {
+  margin: 0;
+  color: black;
+}
+.dark .logo .title {
+  margin: 0;
+  color: white;
+}
+</style>
