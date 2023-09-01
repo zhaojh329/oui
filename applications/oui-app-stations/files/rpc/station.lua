70933b5aa17526b64b9888b86df0b96720d496d0
Jianhui Zhao <zhaojh329@gmail.com>
2023-09-01 23:29:36 +0800

    oui-app-stations: use lua-eco-nl80211 instead of iwinfo-lua
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- applications/oui-app-stations/files/rpc/station.lua
+++ applications/oui-app-stations/files/rpc/station.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-local iwinfo = require 'iwinfo'
+local nl80211 = require 'eco.nl80211'
 local ubus = require 'eco.ubus'
 
 function M.stations()
@@ -8,52 +8,23 @@ function M.stations()
 
     local stations = {}
 
-    for dev_name, dev in pairs(status) do
+    for _, dev in pairs(status) do
         if dev.up then
-            local typ = iwinfo.type(dev_name)
-            if not typ then
-                break
-            end
-
-            local iw = iwinfo[typ]
-
             local band = dev.config.band
 
             for _, ifs in ipairs(dev.interfaces) do
                 local ifname = ifs.ifname
-                local assoclist = iw.assoclist(ifname)
+                local res = nl80211.get_stations(ifname)
 
-                for macaddr, sta in pairs(assoclist) do
+                for _, sta in pairs(res) do
                     stations[#stations + 1] = {
-                        macaddr = macaddr,
+                        macaddr = sta.mac,
                         ifname = ifname,
                         band = band,
                         signal = sta.signal,
                         noise = sta.noise,
-                        rx_rate = {
-                            rate = sta.rx_rate,
-                            mhz = sta.rx_mhz,
-                            mcs = sta.rx_mcs,
-                            ht = sta.rx_ht,
-                            vht = sta.rx_vht,
-                            he = sta.rx_he,
-                            nss = sta.rx_nss,
-                            short_gi = sta.rx_short_gi,
-                            he_gi = sta.rx_he_gi,
-                            he_dcm = sta.rx_he_dcm
-                        },
-                        tx_rate = {
-                            rate = sta.tx_rate,
-                            mhz = sta.tx_mhz,
-                            mcs = sta.tx_mcs,
-                            ht = sta.tx_ht,
-                            vht = sta.tx_vht,
-                            he = sta.tx_he,
-                            nss = sta.tx_nss,
-                            short_gi = sta.tx_short_gi,
-                            he_gi = sta.tx_he_gi,
-                            he_dcm = sta.tx_he_dcm
-                        }
+                        rx_rate = sta.rx_rate,
+                        tx_rate = sta.tx_rate
                     }
                 end
             end
--- applications/oui-app-stations/htdoc/index.vue
+++ applications/oui-app-stations/htdoc/index.vue
@@ -24,9 +24,9 @@ export default {
           render: r => r.macaddr.toUpperCase()
         },
         {
-          title: () => this.$t('Signal / Noise'),
+          title: () => this.$t('Signal'),
           key: 'signal',
-          render: r => `${r.signal} / ${r.noise} dBm`
+          render: r => `${r.signal} dBm`
         },
         {
           title: () => this.$t('RX Rate / TX Rate'),
@@ -42,26 +42,27 @@ export default {
   },
   methods: {
     wifiRate(rate) {
-      let s = (rate.rate / 1000).toFixed(1) + ' Mbit/s' + ', ' + rate.mhz + ' MHz'
+      let s = rate.rate + ' Mbit/s' + ', ' + rate.width + ' MHz'
 
-      if (rate.ht || rate.vht) {
-        if (rate.vht)
-          s += ', VHT-MCS ' + rate.mcs
-        if (rate.nss)
-          s += ', VHT-NSS ' + rate.nss
-        if (rate.ht)
-          s += ', MCS ' + rate.mcs
-      }
-
-      if (rate.he) {
+      if (rate.eht) {
+        s += ', EHT-MCS ' + rate.mcs
+        s += ', EHT-NSS ' + rate.nss
+        s += ', EHT-GI ' + rate.he_gi
+      } else if (rate.he) {
         s += ', HE-MCS ' + rate.mcs
+        s += ', HE-NSS ' + rate.nss
+        s += ', HE-GI ' + rate.he_gi
+        s += ', HE-DCM ' + rate.he_dcm
+      } else if (rate.vht) {
+        s += ', VHT-MCS ' + rate.mcs
         if (rate.nss)
           s += ', HE-NSS ' + rate.nss
-        if (rate.he_gi)
-          s += ', HE-GI ' + rate.he_gi
-        if (rate.he_dcm)
-          s += ', HE-DCM ' + rate.he_dcm
+      } else if (rate.mcs) {
+        s += ', MCS ' + rate.mcs
       }
+
+      if (rate.short_gi)
+        s += ', short GI'
 
       return s
     },
