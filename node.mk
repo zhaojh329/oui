56e9d4ebf3280b77a7832af780434c6ea2d373f2
Jianhui Zhao <zhaojh329@gmail.com>
2024-01-19 23:44:54 +0800

    node.mk: fix code style
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- node.mk
+++ node.mk
@@ -1,9 +1,9 @@
 define ColorInfo
-	tput setaf 6;echo $1;tput sgr0
+  tput setaf 6;echo $1;tput sgr0
 endef
 
 define ColorError
-	tput setaf 1;echo $1;tput sgr0
+  tput setaf 1;echo $1;tput sgr0
 endef
 
 ifneq ($(CONFIG_OUI_USE_HOST_NODE),)
@@ -22,25 +22,22 @@ NODE_VER := $(shell $(NODE_BIN) -v | sed 's/v//')
 endif
 
 define CheckNode
-	@echo
-	@$(call ColorInfo, "Checking Node.js for building oui...")
-	@if [ -n "$(CONFIG_OUI_USE_HOST_NODE)" ]; \
-	then \
-		$(call ColorInfo, "Using Node.js from Host"); \
-	else \
-		$(call ColorInfo, "Using Node.js from OpenWrt"); \
-	fi
-	@if [ -z "$(NODE_VER)" ]; \
-	then \
-		$(call ColorError, "Node.js $(1)+ is required"); \
-		echo;false; \
-	fi
-	@$(call ColorInfo, "Node.js path: $(NODE_BIN)")
-	@$(call ColorInfo, "Node.js version: $(NODE_VER)")
-	@if [ "$(shell echo $(NODE_VER) | awk '{if ($$1 >= $(1)) { printf "ok" }}')" != "ok" ]; \
-	then \
-		$(call ColorError, "Node.js $(1)+ is required"); \
-		echo;false; \
-    fi
-	@echo
+  $(call ColorInfo, "Checking Node.js for building oui..."); \
+  if [ -n "$(CONFIG_OUI_USE_HOST_NODE)" ]; \
+  then \
+      $(call ColorInfo, "Using Node.js from Host"); \
+  else \
+      $(call ColorInfo, "Using Node.js from OpenWrt"); \
+  fi; \
+  if [ -z "$(NODE_VER)" ]; \
+  then \
+    $(call ColorError, "Node.js $(1)+ is required");false; \
+  else \
+    $(call ColorInfo, "Node.js path: $(NODE_BIN)"); \
+    $(call ColorInfo, "Node.js version: $(NODE_VER)"); \
+    if [ "$(shell echo $(NODE_VER) | awk '{if ($$1 >= $(1)) { printf "ok" }}')" != "ok" ]; \
+    then \
+      $(call ColorError, "Node.js $(1)+ is required");false; \
+    fi \
+  fi
 endef
