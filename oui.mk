zhaojh329/oui

commit db9fefbe5872336cbf079f969d68bd402eef85c6

oui.mk: after bundling an application delete its node_modules directory (#272)
node_modules folder is not used by the built application and leaving
it in the build directory consumes scarce filesystem inodes.

When multiple applications are present, it's easy to reach the
filesystem's available inode count.

Signed-off-by: Diego Nieto Cid <dnietoc@gmail.com>

oui.mk
@@ -25,14 +25,15 @@ endef
 define Build/Prepare
 	if [ -d ./htdoc ]; then \
 		$(CP) ./htdoc $(PKG_BUILD_DIR); \
-		echo "VITE_APP_NAME=$(APP_NAME)" > $(PKG_BUILD_DIR)/htdoc/.env.local; \
-		$(NPM) --prefix $(PKG_BUILD_DIR)/htdoc install; \
+		echo "VITE_APP_NAME=$(APP_NAME)" > $(PKG_BUILD_DIR)/htdoc/.env.local
 	fi
 endef
 
 define Build/Compile
 	if [ -d $(PKG_BUILD_DIR)/htdoc ]; then \
-		$(NPM) --prefix $(PKG_BUILD_DIR)/htdoc run build; \
+		$(NPM) --prefix $(PKG_BUILD_DIR)/htdoc install && \
+		$(NPM) --prefix $(PKG_BUILD_DIR)/htdoc run build && \
+		$(RM) -rf $(PKG_BUILD_DIR)/htdoc/node_modules
 	fi
 endef
 

commit ca0dcb100d39385b67de0fce7671b56acba44f99

oui.mk: Fix syntax errors
Fixes: db9fefbe5872336cbf079f969d68bd402eef85c6

Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

oui.mk
@@ -25,7 +25,7 @@ endef
 define Build/Prepare
 	if [ -d ./htdoc ]; then \
 		$(CP) ./htdoc $(PKG_BUILD_DIR); \
-		echo "VITE_APP_NAME=$(APP_NAME)" > $(PKG_BUILD_DIR)/htdoc/.env.local
+		echo "VITE_APP_NAME=$(APP_NAME)" > $(PKG_BUILD_DIR)/htdoc/.env.local; \
 	fi
 endef
 
@@ -33,7 +33,7 @@ define Build/Compile
 	if [ -d $(PKG_BUILD_DIR)/htdoc ]; then \
 		$(NPM) --prefix $(PKG_BUILD_DIR)/htdoc install && \
 		$(NPM) --prefix $(PKG_BUILD_DIR)/htdoc run build && \
-		$(RM) -rf $(PKG_BUILD_DIR)/htdoc/node_modules
+		$(RM) -rf $(PKG_BUILD_DIR)/htdoc/node_modules; \
 	fi
 endef
 